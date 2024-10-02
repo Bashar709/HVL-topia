c05c157ca78e5509c166efd5baa7a0d7396043ec
Bashar989 <bashar.kvs.2004@gmail.com>
2024-10-03 00:12:47 +0200

    om vi skal bruke det so har eg endra litt

--- reg side/hava.js
+++ reg side/hava.js
@@ -1,9 +1,7 @@
 document.getElementById('registrationForm').onsubmit = function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+    event.preventDefault(); 
 
-    // Hide the form
     document.getElementById('registrationForm').style.display = 'none';
 
-    // Show the confirmation message
     document.getElementById('confirmationMessage').style.display = 'block';
 };
